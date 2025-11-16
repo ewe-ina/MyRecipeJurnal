08568ea63fc72a09132c6e13030b801da1238ac1
Ewelina Walkusz <ewelina.walkusz@protonmail.com>
2025-11-16 11:27:13 +0100

    Add pl date
    Signed-off-by: Ewelina Walkusz <ewelina.walkusz@protonmail.com>

--- publishconf.py
+++ publishconf.py
@@ -41,5 +41,30 @@ def add_thumb_flag(generator):
     for article in generator.articles:
         article.has_thumb = thumbs_data.get(article.slug, False)
 
-
 signals.article_generator_finalized.connect(add_thumb_flag)
+
+# -------------------------------
+# Date formatting filter depending on article language
+# -------------------------------
+import locale
+
+def datetimeformat(article, format_pl='%d %B %Y', format_en='%B %d, %Y'):
+    """
+    Format article.date according to its language.
+    - 'pl' -> Polish format
+    - otherwise -> English format
+    """
+    try:
+        if getattr(article, 'lang', 'en') == 'pl':
+            locale.setlocale(locale.LC_TIME, 'pl_PL.UTF-8')
+            return article.date.strftime(format_pl)
+        else:
+            locale.setlocale(locale.LC_TIME, 'en_US.UTF-8')
+            return article.date.strftime(format_en)
+    except locale.Error:
+        return article.date.strftime('%Y-%m-%d')
+
+# Register the filter for Jinja2
+JINJA_FILTERS = {
+    'datetimeformat': datetimeformat,
+}
